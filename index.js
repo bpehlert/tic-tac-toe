e5e8db706bdaea86069c5e3af87c70fb6e15a3ca
Ben Ehlert <benjamin.ehlert@gmail.com>
2018-10-16 20:45:42 -0600

    function for checking for winner

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 // Grab button
 const reset = document.querySelector("button");
-
 // Grab table cells
 const cells = document.querySelectorAll("td");
+// Grab p element to display player turn
+const turn = document.querySelector('p');
+turn.textContent = "Player one's turn."
 
 // Sets player turn indicator
 let player = "O";
-
-// Sets cell index for undo button
-let index = "";
-
-// Function to clear the board
-reset.addEventListener("click", function(){
-  cells.forEach((cell) => {
-    cell.textContent= '';
-  });
-});
 
 // Check each cell value and mark it
 function markCell() {
@@ -23,16 +15,40 @@ function markCell() {
     if (player === "O") {
       this.textContent = "X";
       player = "X";
-      console.log(this.index);
+      turn.textContent = "Player two's turn."
     }else {
       this.textContent = "O";
       player = "O";
-      console.log(this.index);
+      turn.textContent = "Player one's turn."
     }
   }
+  checkForWinner();
 };
 
-// Add event listeners
+const checkForWinner = () => {
+  let winningCombos = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+  ];
+  for (var w = 0; w < winningCombos.length; w++) {
+    if (cells[winningCombos[w][0]].textContent !== ""
+      && cells[winningCombos[w][0]].textContent === cells[winningCombos[w][1]].textContent
+      && cells[winningCombos[w][2]].textContent) {
+      alert("winner");
+    };
+  };
+}
+
+
+// Event listener to reset button to clear the board
+reset.addEventListener("click", function(){
+  cells.forEach((cell) => {
+    cell.textContent= '';
+  });
+});
+
+// Add event listeners for each table cell
 cells.forEach((cell) => {
   cell.addEventListener("click", markCell)
 });
